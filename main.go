fbace946405aa2c86f28e7fd219bd4e726fca38e
ali <aliawada127001@outlook.com>
2023-04-12 02:27:09 +0300

    edit main branch

--- main.go
+++ main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"github.com/XORbit01/DDOS-ARMY/cmd"
-	"log"
-	"os"
 )
 
 func main() {
-	file, err := os.Create("logfile.txt")
-	if err != nil {
-		log.Fatal("Failed to create log file:", err)
-	}
-	defer file.Close()
-
-	// Set the output of the logger to the file
-	log.SetOutput(file)
-
 	cmd.Execute()
 }
