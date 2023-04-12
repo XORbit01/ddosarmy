49cb540e5f5f0606c7bf079fd0715d9f9b12deef
ali <aliawada127001@outlook.com>
2023-04-12 07:20:05 +0300

    separate leader functionality to another file

--- client/client.go
+++ client/client.go
@@ -13,11 +13,6 @@ import (
 	"time"
 )
 
-type Leader struct {
-	Client
-	Password string
-}
-
 type Client struct {
 	http.Client      `json:"-"`
 	Name             string `json:"name"`
@@ -70,58 +65,6 @@ func (c *Client) JoinCamp() error {
 	var msg string
 	_ = json.NewDecoder(do.Body).Decode(&msg)
 	return errors.New(msg)
-}
-
-func (l *Leader) RemoveFromCamp(password string) error {
-	//delete request to dispatcher server /camp/
-	//with body {name: c.Name}
-	rq, err := http.NewRequest("DELETE", l.DispatcherServer+"/camp", nil)
-	rq.Header.Add("Authorization", password)
-
-	if err != nil {
-		return err
-	}
-	do, err := l.Do(rq)
-	defer do.Body.Close()
-	if err != nil {
-		return err
-	}
-	if do.StatusCode == http.StatusOK {
-		return nil
-	} else {
-		//return the body as error message
-		var msg string
-		_ = json.NewDecoder(do.Body).Decode(&msg)
-		return errors.New(msg)
-	}
-}
-
-func (l *Leader) UpdateCampSettings(settings CampSettings) error {
-	//put request to dispatcher server /camp/
-	//convert settings to json and put it in the body
-	jr, err := json.Marshal(settings)
-	if err != nil {
-		return err
-	}
-
-	rq, err := http.NewRequest("PUT", l.DispatcherServer+"/camp", bytes.NewReader(jr))
-	if err != nil {
-		return err
-	}
-	rq.Header.Add("Authorization", l.Password)
-
-	do, err := l.Do(rq)
-	defer do.Body.Close()
-	if err != nil {
-		return err
-	}
-	if do.StatusCode == http.StatusOK {
-		return nil
-	} else {
-		var msg string
-		_ = json.NewDecoder(do.Body).Decode(&msg)
-		return errors.New(msg)
-	}
 }
 
 func (c *Client) ListenAndDo(ChangedDataChan chan CampAPI, logChan chan string) {
@@ -197,73 +140,7 @@ func (c *Client) Start() {
 	}()
 	wg.Wait()
 }
-func (l *Leader) Shutdown() error {
-	// delete request to /system
-	rq, err := http.NewRequest("DELETE", l.DispatcherServer+"/system", nil)
-	if err != nil {
-		return err
-	}
-	rq.Header.Set("Authorization", l.Password)
-	do, err := l.Do(rq)
-	if err != nil {
-		return err
-	}
-	if do.StatusCode == http.StatusOK {
-		return nil
-	}
-	return errors.New("error in shutting down")
-}
-func (l *Leader) ListenChangeView(changedDataChan chan CampAPI) {
-	defer func() {
-		if r := recover(); r != nil {
 
-			color.Red("Dispatcher server is stopped")
-			os.Exit(0)
-		}
-	}()
-	prevCamp := l.GetCamp()
-	changedDataChan <- prevCamp
-	for {
-		camp := l.GetCamp()
-		if !camp.Equals(prevCamp) {
-			select {
-			case <-changedDataChan:
-			default:
-			}
-			changedDataChan <- camp
-		}
-		time.Sleep(1 * time.Second)
-	}
-}
-func (l *Leader) Start() {
-	func() {
-		defer func() {
-			if r := recover(); r != nil {
-				color.Red("Dispatcher server is not running")
-				os.Exit(0)
-			}
-		}()
-		err := l.Ping()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	var changedDataChan = make(chan CampAPI, 1)
-	logChan := make(chan string, 10)
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	go func() {
-		l.ListenChangeView(changedDataChan)
-		wg.Done()
-	}()
-	go func() {
-		l.StartLeaderView(changedDataChan, logChan)
-		wg.Done()
-	}()
-	wg.Wait()
-}
 func (c *Client) Ping() error {
 	rq, err := http.NewRequest("GET", c.DispatcherServer+"/ping", nil)
 	if err != nil {
